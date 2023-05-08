3adfaaea6b31c6a44e5bebb5b8f36ad584a68fa0
Tuhin Samanta <tuhinsamantastudent@gmail.com>
2023-05-08 21:45:30 +0530

    changes

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
-console.log("--Logging....");
-console.log(process.argv, process);
-//process.exit(1);
+const { exec } = require("child_process");
+const { exit } = require("process");
+
+exec("cat $1", (err, stdout, stderr) => {
+  console.log({ stdout, err, stderr });
+});
+
+exec("git rev-parse --abbrev-ref HEAD", (err, stdout, stderr) => {
+  if (err || stderr) {
+    (err && console.error(err)) || (stderr && console.log(stderr));
+    exit(1);
+  } else {
+    console.log(process.argv);
+    exit(1);
+  }
+});
